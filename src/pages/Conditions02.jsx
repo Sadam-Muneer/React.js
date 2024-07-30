6278eb0a8eda2dc5ddb4dd06a2a5df37293e4639
Muhammad Sadam Muneer <sadammuneer39@gmail.com>
2024-07-30 10:37:03 +0500

    conditions

--- src/pages/Conditions02.jsx
+++ src/pages/Conditions02.jsx
@@ -2,10 +2,22 @@ import React from "react";
 
 const Conditions02 = () => {
   const age = 18;
+  let canwatch = "Not Available";
+  if (age >= 18) canwatch = "Watch Now";
+  const lastmethod = () => {
+    if (age >= 18) "Watch Now";
+    return "Not Available";
+  };
   return (
     <div>
-      <button className="bg-red-800 border-red-100 rounded-md p-3">
+      <button className="bg-red-800 border-red-100 rounded-md p-3 ">
         {age <= 20 ? "Watch Now" : "Not Available"}
+      </button>
+      <button className="bg-red-800 border-red-100 rounded-md p-3 mx-4">
+        {canwatch}
+      </button>
+      <button className="bg-red-800 border-red-100 rounded-md p-3 mx-4">
+        {lastmethod()}
       </button>
     </div>
   );
