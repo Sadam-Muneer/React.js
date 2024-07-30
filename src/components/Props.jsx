0e601d030eb6feaa34be06cd5ad48082fcd22f46
Muhammad Sadam Muneer <sadammuneer39@gmail.com>
2024-07-30 16:46:14 +0500

    destructuring

--- src/components/Props.jsx
+++ src/components/Props.jsx
@@ -1,33 +1,34 @@
 import React from "react";
 
 const Props = (sadam) => {
-  if (!sadam.series) {
+  const { series } = sadam;
+  if (!series) {
     return <div>Loading...</div>;
   }
   return (
     <div>
       <div className="bg-white shadow-lg rounded-lg overflow-hidden ">
         <img
-          src={sadam.series.image_url}
-          alt={sadam.series.name}
+          src={series.image_url}
+          alt={series.name}
           className="w-full h-48 object-cover"
         />
         <div className="p-4">
           <h2 className="text-lg ">
-            <b>Name</b>:{sadam.series.name}
+            <b>Name</b>:{series.name}
           </h2>
           <p className="text-gray-600">
-            <b>Rating:</b> {sadam.series.rating}
+            <b>Rating:</b> {series.rating}
           </p>
           <p className="text-gray-700 mt-2">
             <b>Description : </b>
-            {sadam.series.description}
+            {series.description}
           </p>
           <p className="text-gray-500 mt-2">
-            <b>Cast </b>: {sadam.series.cast.join(", ")}
+            <b>Cast </b>: {series.cast.join(", ")}
           </p>
           <a
-            href={sadam.series.watchButton}
+            href={series.watchButton}
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
