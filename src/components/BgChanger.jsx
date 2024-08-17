d50039f9dbc103a0a8d6fa773f04eca9494c17e7
Muhammad Sadam Muneer <sadammuneer39@gmail.com>
2024-08-17 18:36:17 +0500

    background changer

--- src/components/BgChanger.jsx
+++ src/components/BgChanger.jsx
@@ -4,15 +4,21 @@ const BgChanger = () => {
   const [color, setColor] = useState("white");
   return (
     <div
-      className="outline-dashed text-blue-900 border"
+      className="outline-dashed text-blue-900 border mr-2"
       style={{ background: color, height: "100vh" }}
     >
       <>
         <button
-          className="bg-black text-white px-3 py-2 rounded-sm mr-2"
+          className="bg-white text-black px-3 py-2 rounded-sm mr-2"
           onClick={() => setColor("white")}
         >
-          Restore
+          White
+        </button>
+        <button
+          className="bg-black text-white px-3 py-2 rounded-sm mr-2"
+          onClick={() => setColor("black")}
+        >
+          Black
         </button>
         <button
           className="bg-red-900 text-white px-3 py-2 rounded-sm mr-2"
@@ -31,6 +37,12 @@ const BgChanger = () => {
           onClick={() => setColor("blue")}
         >
           Blue
+        </button>
+        <button
+          className="bg-yellow-400 text-white px-3 py-2 rounded-sm mr-2"
+          onClick={() => setColor("yellow")}
+        >
+          Yellow
         </button>
       </>
     </div>
