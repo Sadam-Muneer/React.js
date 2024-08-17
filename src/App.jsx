963271d98d51d9d8a3c153e2f40a46a2eaa58e56
Muhammad Sadam Muneer <sadammuneer39@gmail.com>
2024-08-17 18:37:15 +0500

    background changer

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-// import Basics01 from "./components/Basics01";
-// import Conditions02 from "./components/Conditions02";
-// import UseState from "./components/UseStates.jsx";
-// import Interview03 from "./components/Interview03";
-// import Map04 from "./components/Loop.jsx";
-// import Props from "./components/Props.jsx";
-// import UseEffect from "./components/UseEffect.jsx";
-// import Interview02 from "./components/Interview2.jsx";
+import Basics01 from "./components/Basics01";
+import Conditions02 from "./components/Conditions02";
+import UseState from "./components/UseStates.jsx";
+import Interview03 from "./components/Interview03";
+import Map04 from "./components/Loop.jsx";
+import Props from "./components/Props.jsx";
+import UseEffect from "./components/UseEffect.jsx";
+import Interview02 from "./components/Interview2.jsx";
 import BgChanger from "./components/BgChanger.jsx";
 
 const App = () => {
   return (
     <>
-      {/* <Basics01 />
+      <Basics01 />
       <Conditions02 />
       <Interview03 />
       <Map04 />
       <Props />
       <UseState />
       <UseEffect />
-      <Interview02 /> */}
+      <Interview02 />
       <BgChanger />
     </>
   );
